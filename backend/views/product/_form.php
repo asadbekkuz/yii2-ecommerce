2d70d362041322fe97142fcaeaccd3d46b610039
webcoderphp <ALn5T?%t?i.aF%e>
2022-10-25 23:26:38 +0500

    Create Product Crud and Controller,Search model, Views

---
+++ backend/views/product/_form.php
@@ -0,0 +1,51 @@
+<?php
+
+use dosamigos\ckeditor\CKEditor;
+use yii\helpers\Html;
+use yii\bootstrap5\ActiveForm;
+
+/** @var yii\web\View $this */
+/** @var common\models\Product $model */
+/** @var yii\widgets\ActiveForm $form */
+?>
+
+<div class="product-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'description')->widget(CKEditor::class, [
+        'options' => ['rows' => 6],
+        'preset' => 'basic'
+    ]) ?>
+
+    <?= $form->field($model, 'image', [
+        'template' => '
+                <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text" id="inputGroupFileAddon01">Upload</span>
+                    </div>
+                    <div class="custom-file">
+                        {input}
+                        {label}
+                    </div>
+                </div>
+        ',
+        'inputOptions'=>['class'=>'custom-file-input'],
+        'labelOptions'=>['class'=>'custom-file-label']
+        ])->fileInput() ?>
+
+    <?= $form->field($model, 'price')->textInput([
+        'type' => 'number'
+    ]) ?>
+
+    <?= $form->field($model, 'status')->checkbox() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
