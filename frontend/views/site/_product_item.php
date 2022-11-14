536808ce59a5266bd4864ce4e3e0b2c5a4681a0b
webcoderphp <ALn5T?%t?i.aF%e>
2022-11-14 12:57:04 +0500

    Change product card width

--- frontend/views/site/_product_item.php
+++ frontend/views/site/_product_item.php
@@ -6,8 +6,8 @@
  */
 /** @var \common\models\Product $model */
 ?>
-<div class="card" style="width:304px; height: 450px">
-    <img src="<?php echo $model->getImgUrl() ?>" style="width:302px;height: 194px; object-fit: fill" class="card-img-top" alt="...">
+<div class="card" style="width:304px">
+    <img src="<?php echo $model->getImgUrl() ?>" style="width:302px;height: 315px; object-fit: fill" class="card-img-top" alt="...">
     <div class="card-body">
         <h5 class="card-title"><?php echo \yii\helpers\StringHelper::truncateWords($model->name,20) ?></h5>
         <p class="card-text" style="width: 270px;height: 96px"><?php echo $model->getShortDescription() ?></p>
