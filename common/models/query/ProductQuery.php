b3898a56f67fb7bc389ee9a3e0b5729bd5df6741
webcoderphp <ALn5T?%t?i.aF%e>
2022-11-14 19:59:58 +0500

    Add id method to ProductQuery.php

--- common/models/query/ProductQuery.php
+++ common/models/query/ProductQuery.php
@@ -36,4 +36,9 @@ class ProductQuery extends \yii\db\ActiveQuery
     {
         return $this->andWhere(['status'=>1]);
     }
+
+    public function id($productId)
+    {
+        return $this->andWhere(['id'=>$productId]);
+    }
 }
