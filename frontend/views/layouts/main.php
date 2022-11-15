49c7f264a07aeff20d5644bb08f83d0948e81a24
webcoderphp <ALn5T?%t?i.aF%e>
2022-11-15 15:18:29 +0500

    Fix the bug in navbar.

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -1,6 +1,7 @@
 <?php
 
 /** @var \yii\web\View $this */
+
 /** @var string $content */
 
 use common\widgets\Alert;
@@ -13,77 +14,75 @@ $cartItems = $this->params['cartItem'];
 AppAsset::register($this);
 ?>
 <?php $this->beginPage() ?>
-<!DOCTYPE html>
-<html lang="<?= Yii::$app->language ?>" class="h-100">
-<head>
-    <meta charset="<?= Yii::$app->charset ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <?php $this->registerCsrfMetaTags() ?>
-    <title><?= Html::encode($this->title) ?></title>
-    <?php $this->head() ?>
-</head>
-<body class="d-flex flex-column h-100">
-<?php $this->beginBody() ?>
+    <!DOCTYPE html>
+    <html lang="<?= Yii::$app->language ?>" class="h-100">
+    <head>
+        <meta charset="<?= Yii::$app->charset ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <?php $this->registerCsrfMetaTags() ?>
+        <title><?= Html::encode($this->title) ?></title>
+        <?php $this->head() ?>
+    </head>
+    <body class="d-flex flex-column h-100">
+    <?php $this->beginBody() ?>
 
-<header>
-    <?php
-    NavBar::begin([
-        'brandLabel' => Yii::$app->name,
-        'brandUrl' => Yii::$app->homeUrl,
-        'options' => [
-            'class' => 'navbar navbar-expand-md  navbar-dark bg-dark fixed-top',
-        ],
-    ]);
-    $menuItems[] = [
-        'label' => "Cart <span class='badge bg-danger' id='product-counter'>".$cartItems."</span>",
-        'url' => ['/cart/index'],
-    ];
-    if (Yii::$app->user->isGuest) {
-        $menuItems=[
-            ['label' => 'Signup', 'url' => ['/site/signup']],
-            ['label' => 'Login', 'url' => ['/site/login']],
+    <header>
+        <?php
+        NavBar::begin([
+            'brandLabel' => Yii::$app->name,
+            'brandUrl' => Yii::$app->homeUrl,
+            'options' => [
+                'class' => 'navbar navbar-expand-md  navbar-dark bg-dark fixed-top',
+            ],
+        ]);
+        $menuItems[] = [
+            'label' => "Cart <span class='badge bg-danger' id='product-counter'>" . $cartItems . "</span>",
+            'url' => ['/cart/index'],
         ];
-    }else{
-        $menuItems[] =[
-            'label' => Yii::$app->user->identity->getDisplayName(),
-            'items'=>[
-                ['label' => 'Profile', 'url' => ['/profile/index']],
-                [
-                    'label' => 'Logout',
-                    'linkOptions'=>[
-                            'data-method'=>'post'
-                    ],
-                    'url' => ['/site/logout']],
-            ]
-        ];
-    }
+        if (Yii::$app->user->isGuest) {
+            $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
+            $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
+        } else {
+            $menuItems[] = [
+                'label' => Yii::$app->user->identity->getDisplayName(),
+                'items' => [
+                    ['label' => 'Profile', 'url' => ['/profile/index']],
+                    [
+                        'label' => 'Logout',
+                        'linkOptions' => [
+                            'data-method' => 'post'
+                        ],
+                        'url' => ['/site/logout']],
+                ]
+            ];
+        }
 
-    echo Nav::widget([
-        'options' => [
+        echo Nav::widget([
+            'options' => [
                 'class' => 'navbar-nav ms-auto',
-        ],
-        'items' => $menuItems,
-        'encodeLabels'=> false,
-    ]);
-    NavBar::end();
-    ?>
-</header>
+            ],
+            'items' => $menuItems,
+            'encodeLabels' => false,
+        ]);
+        NavBar::end();
+        ?>
+    </header>
 
-<main role="main" class="flex-shrink-0">
-    <div class="container">
-        <?= Alert::widget() ?>
-        <?= $content ?>
-    </div>
-</main>
+    <main role="main" class="flex-shrink-0">
+        <div class="container">
+            <?= Alert::widget() ?>
+            <?= $content ?>
+        </div>
+    </main>
 
-<footer class="footer mt-auto py-3 text-muted">
-    <div class="container">
-        <p class="float-start">&copy; <?= Html::encode(Yii::$app->name) ?> <?= date('Y') ?></p>
-        <p class="float-end"><?= Yii::powered() ?></p>
-    </div>
-</footer>
+    <footer class="footer mt-auto py-3 text-muted">
+        <div class="container">
+            <p class="float-start">&copy; <?= Html::encode(Yii::$app->name) ?> <?= date('Y') ?></p>
+            <p class="float-end"><?= Yii::powered() ?></p>
+        </div>
+    </footer>
 
-<?php $this->endBody() ?>
-</body>
-</html>
+    <?php $this->endBody() ?>
+    </body>
+    </html>
 <?php $this->endPage();
