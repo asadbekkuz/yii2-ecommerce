6493ceeead090b2cfca45d8cfa294bb55ac0ea88
webcoderphp <ALn5T?%t?i.aF%e>
2022-11-24 13:50:00 +0500

    Paypal Checkout Button Integrations

--- frontend/views/cart/checkout.php
+++ frontend/views/cart/checkout.php
@@ -10,6 +10,8 @@ use yii\bootstrap5\ActiveForm;
 /** @var \common\models\Product $totalPrice */
 
 ?>
+<script src="https://www.paypal.com/sdk/js?client-id=test&currency=USD"></script>
+
 <?php $form = ActiveForm::begin(); ?>
 <div class="row">
     <div class="col">
@@ -68,12 +70,47 @@ use yii\bootstrap5\ActiveForm;
                             </tr>
                         </table>
                     </div>
-                    <div class="card-body text-end">
-                        <?php echo \yii\helpers\Html::submitButton('Checkout',['class'=>'btn btn-secondary']) ?>
-                    </div>
+                    <div id="paypal-button-container"></div>
+<!--                    <div class="card-body text-end">-->
+<!--                        --><?php //echo \yii\helpers\Html::submitButton('Checkout',['class'=>'btn btn-secondary']) ?>
+<!--                    </div>-->
                 </div>
             </div>
         </div>
     </div>
 </div>
 <?php ActiveForm::end(); ?>
+<script>
+    // Render the PayPal button into #paypal-button-container
+    paypal.Buttons({
+
+        // Set up the transaction
+        createOrder: function(data, actions) {
+            return actions.order.create({
+                purchase_units: [{
+                    amount: {
+                        value: '88.44'
+                    }
+                }]
+            });
+        },
+
+        // Finalize the transaction
+        onApprove: function(data, actions) {
+            return actions.order.capture().then(function(orderData) {
+                // Successful capture! For demo purposes:
+                console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
+                var transaction = orderData.purchase_units[0].payments.captures[0];
+                alert('Transaction '+ transaction.status + ': ' + transaction.id + '\n\nSee console for all available details');
+
+                // Replace the above to show a success message within this page, e.g.
+                // const element = document.getElementById('paypal-button-container');
+                // element.innerHTML = '';
+                // element.innerHTML = '<h3>Thank you for your payment!</h3>';
+                // Or go to another URL:  actions.redirect('thank_you.html');
+            });
+        }
+
+
+    }).render('#paypal-button-container');
+</script>
